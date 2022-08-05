c9c6180e04acc580e6e1cb23c99517ad45ec65ff
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-08-05 11:00:29 -0300

    I do what i can ok?

---
+++ Assets/DropNET.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropNET : MonoBehaviour
+{
+    public GameObject compu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void OnCollissionEnter(Collision col)
+    {
+        if (col.gameObject.name == "FPSController" & Input.GetKeyDown(KeyCode.N))
+        {
+            Instantiate (compu);
+        }
+    }
+}
